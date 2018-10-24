c8b9eba8681cdd742f83a53a2767e6f5576591e9
yangping4271 <yangping4271163.com>
2018-10-24 15:31:02 +0800

    优化

---
+++ views.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.route('/index')
+def index():
+        return 'hello world!'
+
+app.route('/login')
+def login():
+    return 'login page'
+
+if '__name__' = '__main__':
+    app.run()
