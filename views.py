57ede32e388c1a8c5076ac65b4323f5d15d30ea6
YANGPING <yangping4271@gmail.com>
2020-04-21 17:37:02 +0800

    格式化。

--- views.py
+++ views.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 app.route('/index')
+
+
 def index():
-        return 'hello world!'
+    return 'hello world!'
+
 
 app.route('/login')
+
+
 def login():
     return 'login page'
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     app.run()
